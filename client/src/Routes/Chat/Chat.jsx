60fc369a90f5fc673f4741ae6658926ff76330a0
Chaewon Kong <chaewonkong@gmail.com>
2019-05-24 00:13:12 +0900

    fix: change socket server path in Chat

--- client/src/Routes/Chat/Chat.jsx
+++ client/src/Routes/Chat/Chat.jsx
@@ -9,7 +9,7 @@ import { highlight, languages } from "prismjs/components/prism-core";
 import "prismjs/components/prism-clike";
 import "prismjs/components/prism-javascript";
 
-let path = window.location.toString();
+let path = "https://codechats.herokuapp.com/";
 if (path.match("localhost")) path = "http://localhost:5000";
 
 // const socket = io.connect(path);
